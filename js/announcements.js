839bd9ec33408a2111eb849783af4ca380470973
Gonah <kvnhawker@gmail.com>
2016-05-04 19:01:48 -0400

    announcements

--- js/announcements.js
+++ js/announcements.js
@@ -5,14 +5,17 @@ $.get(announceSpreadsheet, function(data) {
 
 function updateAnnouncements(responseJson) {
   var div = $('#' + "announcements");
-  console.log(div);
   var numEntries = responseJson.feed.entry.length;
-  console.log(numEntries);
   for (var i = numEntries; (i > 0 && numEntries-i < 5); i--) {
     var row = responseJson.feed.entry[i-1];
     var date = row.gsx$date.$t;
     var title = row.gsx$title.$t;
     var announcement = row.gsx$announcement.$t;
-    div.append("<p><b>"+date+"</b>: "+title+"	<br /> "+announcement+"</p>");
+    var link = row.gsx$link.$t;
+    var linkHtml = "";
+    if(link.length > 0) {
+      linkHtml = '<br /><a class="announcelink" href="'+link+'">More info...</a>';
+    }
+    div.append('<p class="announcement"><b>'+date+'</b>: '+title+'  <br /> '+announcement+''+linkHtml+'</p>');
   }
 }
